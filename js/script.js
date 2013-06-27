dc527c76109240a727bc4a025d875c2ca35e33f1
Dan Giulvezan <dan.giulvezan@gmail.com>
2013-06-27 09:52:13 -0700

    - Added support for using the main API key to load stats for all monitors (still supports keys for individual monitors as well) - Now dynamically creating the HTML for the charts so multiple monitors can be displayed at once without having to be hard coded - Renamed a couple vars - The number of days to use for the custom uptime stats is now a var so it's easier to change - Minor style tweaks to the charts

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
 /**
-* @param {String} apiKey The monitor-specific api key with UptimeRobot to use
+* @param {String} apiKey The api key with UptimeRobot to use (can be the main api key or a monitor-specific one)
 * @param {String} statusURL The URL to check
 * @param {String} data The data to pass to the api in the ajax request
+* @param {String} customUptimeDays The uptime percentage for a specific number of days (e.g., the last 30 days)
 */
 var apiKey = '[YOUR_API_KEY]';
 var statusURL = 'http://api.uptimerobot.com/getMonitors';
 var uptimeForLabel = 'Google.com';
-var data = {apiKey:apiKey, logs:1, alertContacts:1, customUptimeRatio: 30, format:'json'};
-
-$('.chart').easyPieChart({
-	animate: 2000,
-	lineWidth: 10,
-	size: 270,
-	lineCap: 'square',
-	barColor: '#88B8E6'
-});
-
-$('.chart-label').text('Alltime Uptime for: ' + uptimeForLabel);
+var customUptimeDays = 30;
+var data = {apiKey:apiKey, logs:1, alertContacts:1, customUptimeRatio: customUptimeDays, format:'json'};
 
 /**
 * The callback function for the response
 */			
 function jsonUptimeRobotApi(e) {
-	var alltimeUpdateRatio = e.monitors.monitor[0].alltimeuptimeratio;
+	var html = '';
+	var updateQueue = [];
+	var count = 0;
+	$.each(e.monitors.monitor, function(e) {
+		var alltimeUptimeRatio = this.alltimeuptimeratio;
+		var customUptimeRatio = this.customuptimeratio;
+		var siteName = this.friendlyname;
+		var dateTime = this.log[0].datetime;
+		html += '<div class="label chart-label">Total Uptime for ' + siteName + '</div><div class="chart all-' + e + '"><div class="percentage" data-percent="' + alltimeUptimeRatio + '"><span>' + alltimeUptimeRatio + '%</span></div></div>';
+		html += '<div class="label chart-label">' + customUptimeDays + '-Day Uptime for ' + siteName + '</div><div class="chart custom-' + e + '"><div class="percentage" data-percent="' + customUptimeRatio + '"><span>' + customUptimeRatio + '%</span></div></div>';
+		
+		updateQueue.push({'class': '.chart.all-' + e, 'value': alltimeUptimeRatio});
+		updateQueue.push({'class': '.chart.custom-' + e, 'value': customUptimeRatio});
+	});
 	
-	$('.percentage span').text(alltimeUpdateRatio + '%');
-	$('.chart').data('easyPieChart').update(alltimeUpdateRatio);
+	$('.container').html(html);
+	html = null;
+	
+	$('.chart').easyPieChart({
+		animate: 1600,
+		lineWidth: 10,
+		size: 130,
+		lineCap: 'square',
+		barColor: '#88B8E6'
+	});
+	
+	$.each(updateQueue, function(e, r) {
+		//console.log(r.class);
+		$(r.class).data('easyPieChart').update('100');
+	});
 };
 
 $.ajax({
